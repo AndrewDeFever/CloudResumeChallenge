1998b3cdcca3b8926dc70cafb5f3f96865b50abd
Andrew DeFever <andrewdefever@gmail.com>
2025-07-24 18:52:54 -0500

    Update test_geo.py
    Updated to add region

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -1,18 +1,21 @@
+import os
 import json
 from GeoTracker import lambda_handler
 
+# Set default AWS region so boto3 doesn't error
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
 def test_lambda_handler_returns_200():
-    # Simulate an API Gateway event with dummy data
-    event = {
+    mock_event = {
         "requestContext": {
             "identity": {
                 "sourceIp": "123.123.123.123"
             }
         }
     }
-    context = {}
+    mock_context = {}
 
-    response = lambda_handler(event, context)
+    response = lambda_handler(mock_event, mock_context)
     assert response["statusCode"] == 200
     body = json.loads(response["body"])
     assert "message" in body
