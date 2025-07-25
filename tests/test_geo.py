e6e4d3565b051ca636e6e5d49a10abc14dea03b6
Andrew DeFever <andrewdefever@gmail.com>
2025-07-25 09:56:44 -0500

    Update test_geo.py

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -2,18 +2,17 @@ import os
 import json
 from GeoTracker import lambda_handler
 
-# Set default AWS region so boto3 doesn't error
-os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+# Set required env vars
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["DYNAMO_TABLE_NAME"] = "GeoVisitors"
 
 def test_lambda_handler_returns_200():
     mock_event = {
-        "requestContext": {
-            "identity": {
-                "sourceIp": "123.123.123.123"
-            }
+        "headers": {
+            "X-Forwarded-For": "123.123.123.123"
         }
     }
+
     mock_context = {}
 
     response = lambda_handler(mock_event, mock_context)
